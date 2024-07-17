4cff0b0d2f8eb5557858034935f538e9e1dc74a7
sathwikreddy122 <venkatasathwik12604@gmail.com>
2024-07-18 00:58:57 +0530

    Project cw

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,18 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/home/sahilz/Desktop/Open Resume/open-resume/src/app/layout.tsx": [
+      "/home/sahilz/Desktop/Open Resume/open-resume/src/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/home/sahilz/Desktop/Open Resume/open-resume/src/app/page": [
+      "/home/sahilz/Desktop/Open Resume/open-resume/src/app/globals.css"
+    ],
+    "/home/sahilz/Desktop/Open Resume/open-resume/src/app/resume-import/page": [
+      "/home/sahilz/Desktop/Open Resume/open-resume/src/app/globals.css"
+    ],
+    "/home/sahilz/Desktop/Open Resume/open-resume/src/app/resume-builder/page": [
+      "/home/sahilz/Desktop/Open Resume/open-resume/src/app/globals.css"
+    ]
+  }
+}

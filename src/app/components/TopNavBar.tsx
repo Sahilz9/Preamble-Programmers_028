2242ad0859f2c7389d2aef50d544898e6c75b9e0
Sahilz9 <sahilyadav9857@gmail.com>
2024-07-20 23:13:30 +0530

    Authentication Updated

--- src/app/components/TopNavBar.tsx
+++ src/app/components/TopNavBar.tsx
@@ -37,7 +37,7 @@ export const TopNavBar = () => {
             <Link
               key={text}
               className="rounded-md px-1.5 py-2 text-gray-500 hover:bg-gray-100 focus-visible:bg-gray-100 lg:px-4"
-              href="/signin"
+              href={href}
             >
               {text}
             </Link>

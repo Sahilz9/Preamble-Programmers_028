2ba84142bc828cddcb457263c2291059fb71b5e1
Sahilz9 <sahilyadav9857@gmail.com>
2024-07-22 13:57:08 +0530

    accordion updated

--- src/app/home/QuestionsAndAnswers.tsx
+++ src/app/home/QuestionsAndAnswers.tsx
@@ -41,24 +41,24 @@ export const QAS = () => {
 
   return (
     <div className="px-4 py-12 lg:container md:px-6 md:py-12 lg:mx-auto lg:py-12">
-      <h1 className="text-center  text-3xl font-bold leading-7 text-gray-800 dark:text-white lg:text-4xl lg:leading-9">
+      <h1 className="text-center  text-3xl font-bold leading-7 text-gray-800 dark:text-black lg:text-4xl lg:leading-9">
         FAQ's
       </h1>
 
-      <div className="mx-auto mt-8 w-full bg-gray-100 px-4 py-6 dark:bg-gray-800 md:mt-10 md:p-6 lg:mt-12 lg:w-8/12 lg:px-6 lg:py-7">
+      <div className="mx-auto mt-8 w-full bg-gray-100 px-4 py-6 dark:bg-gray-300 md:mt-10 md:p-6 lg:mt-12 lg:w-8/12 lg:px-6 lg:py-7 rounded-xl">
         <div className="flex flex-col justify-between md:flex-row">
           <div className="mb-8 text-center md:mb-0 md:text-left">
-            <h2 className="mb-4 text-xl font-medium leading-5 text-gray-800 dark:text-white lg:mb-2">
+            <h2 className="mb-4 text-xl font-medium leading-5 text-gray-300 dark:text-black lg:mb-2">
               Questions
             </h2>
-            <p className="mx-auto w-11/12 text-sm font-normal leading-5 text-gray-600 dark:text-gray-300 md:ml-0 md:w-8/12">
+            <p className="mx-auto w-11/12 text-sm font-normal leading-5 text-black md:ml-0 md:w-8/12">
               If you don’t find your answer, please contact us or leave a
               message, we’ll be more than happy to assist you.
             </p>
           </div>
 
           <div className="flex items-center justify-center">
-            <div className="flex w-full items-center justify-between bg-white px-4 py-3 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-offset-2 md:justify-center">
+            <div className="flex w-full items-center justify-between rounded-md bg-white px-4 py-3 focus:outline-none focus:ring-2 focus:ring-gray-800 focus:ring-offset-2 md:justify-center">
               <input
                 className="bg-white focus:outline-none"
                 type="text"
@@ -84,11 +84,11 @@ export const QAS = () => {
               >
                 <div>
                   <p
-                    className={`flex items-center justify-center text-base font-medium leading-6 text-gray-800 dark:text-white md:leading-4 ${
+                    className={`flex items-center justify-center text-base font-medium leading-6 text-gray-800 dark:text-black md:leading-4 ${
                       visibleAnswers[index] ? "font-semibold" : ""
                     }`}
                   >
-                    <span className="mr-4 text-lg font-semibold leading-6 text-gray-800 dark:text-white md:text-xl md:leading-5 lg:mr-6 lg:text-2xl lg:leading-4">
+                    <span className="mr-4 text-lg font-semibold leading-6 text-gray-800 dark:text-black md:text-xl md:leading-5 lg:mr-6 lg:text-2xl lg:leading-4">
                       Q{index + 1}.
                     </span>
                     {item.question}
@@ -108,7 +108,7 @@ export const QAS = () => {
                     alt="toggler"
                   />
                   <img
-                    className={`hidden transform dark:block ${
+                    className={`hidden transform dark:block bg-gray-300 rounded-full ${
                       visibleAnswers[index] ? "rotate-180" : ""
                     }`}
                     src="https://tuk-cdn.s3.amazonaws.com/can-uploader/faq-8-svg2dark.svg"
@@ -123,7 +123,7 @@ export const QAS = () => {
                   visibleAnswers[index] ? "" : "hidden"
                 }`}
               >
-                <p className="text-base font-normal leading-6 text-gray-600 dark:text-gray-300">
+                <p className="text-base font-normal leading-6 text-gray-600 dark:text-gray-600">
                   {item.answer}
                 </p>
               </div>
